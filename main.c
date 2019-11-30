4869a606e3a313dc25db8a36c155880fa2d92ae1
Kensei Yarimizu <kensei@abeja.asia>
2019-12-01 02:12:45 +0900

    insert preceeding elements

--- main.c
+++ main.c
@@ -20,56 +20,31 @@ void insert(struct linkedlist *l, int n) {
         *l->first = nd;
         return;
     }
-    /*
     struct node *last = l->first;
-    while (last != NULL) {
+    while (last->next != NULL) {
         printf("skip\n");
         last = last->next;
     }
-    last = (struct node *)malloc(sizeof(struct node));
-    *last = nd;
-
-    if (l->first == NULL) {
-        printf("l->first is NULL\n");
-    } else {
-        printf("l->first is not NULL\n");
-    }
-    */
+    printf("last node is %d\n", last->val);
+    last->next = (struct node *)malloc(sizeof(struct node));
+    *last->next = nd;
 };
 
 int main() {
     printf("hello world\n");
     struct linkedlist l = {};
-    if (l.first == NULL) {
-        printf("first is NULL\n");
-    } else {
-        printf("first is not NULL\n");
-    }
 
     insert(&l, 3);
-    printf("----------------------------");
+    insert(&l, 4);
+    insert(&l, 5);
+    printf("----------------------------\n");
 
     struct node *current = l.first;
     int count = 0;
     while (current != NULL) {
-        printf("hello\n");
         count++;
         printf("val is %d\n", current->val);
-        printf("count %d\n", count);
-        if (current->next == NULL) {
-            printf("current->next == NULL");
-            break;
-        }
-        if (current == NULL) {
-            printf("current == NULL");
-            break;
-        }
-        printf("world\n");
         struct node *nn = current->next;
-        if (nn == NULL) {
-            printf("nn == NULL");
-            break;
-        }
         current = nn;
     }
 }
